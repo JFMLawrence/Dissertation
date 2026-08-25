48cecab92732af761f17a800577234356adf3f98
JFMLawrence <julawrence15@gmail.com>
2026-08-25 15:17:12 +0100

    Add files via upload

---
+++ S04download_genome_and_gtf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p shared # You select the queue(cluster) here
+#SBATCH -c 1                # number of CPU cores to allocate, one per thread, up to 128.
+#SBATCH --mem=2G            # memory required, in units of k,M or G, up to 250G.
+#SBATCH --gres=tmp:3G       # $TMPDIR space required on each compute node, up to 400G.
+#SBATCH -t 01:00:00     # time limit in format dd-hh:mm:ss
+#SBATCH --job-name=S04download_genome_and_gtf # This name will let you follow your job
+#SBATCH --output=../log/S04download_genome_and_gtf%A_%a.out
+#SBATCH --error=../log/S04download_genome_and_gtf%A_%a.err
+#SBATCH --array=1
+cd ~/diss
+work_dir="genome"
+if [ ! -d $work_dir ]
+then
+    mkdir $work_dir
+fi
+cd ${work_dir}
+wget -nc https://ftp.ensembl.org/pub/release-116/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+wget -nc https://ftp.ensembl.org/pub/release-116/gtf/homo_sapiens/Homo_sapiens.GRCh38.116.gtf.gz
